a7d208080a8c41c806c64538a951a73dc8093720
Nithya <jaynithya123@gmail.com>
2021-01-01 15:08:26 -0500

    Finished play

--- play.py
+++ play.py
@@ -5,14 +5,42 @@ from cards import Card
 sys.path.append('war')
 from war import War
 
+print("Games to play: War")
+game = input("What game do you want to play?\n")
+if game == "War":
+    players = []
 
-players = []
-for i in range(1, len(sys.argv)):
-    players.append(Player(str(sys.argv[i])))
+    print("LET'S PLAY WAR! ENTER 1 TO EXIT! ENTER PLAYERS:")
+    name = ""
+    while not name == "1":
+        name = input()
+        if not name == "1":
+            players.append(Player(name))
+    print("HERE ARE YOUR PLAYERS:")
+    for player in players:
+        print(player.name)
 
-game = War(players)
-for player in players:
-    print(player.name, len(player.hand))
+
+    game = War(players)
+    while len(players[0].hand) > 0:
+        nextMove = input("Enter 'move' to make the next move, enter 'hands' to check how many cards are in each player's hands,\n or enter 'piles' to see how many cards are in each player's piles.\n")
+        if nextMove == "move":
+            game.playRound()
+        elif nextMove == "hands":
+            for player in players:
+                print(player.name + " has " + str(len(player.hand)) + " cards left.")
+        elif nextMove == "piles":
+            for player in players:
+                print(player.name + " has " + str(len(player.pile)) + " cards in their discard pile.")
+        else:
+            print("Not a valid move")
+        print("\n\n\n")
+    print("Here are the winners: ")
+    for player in game.winners():
+        print(player.name + " with " + str(len(player.pile)) + " cards in their pile.")
+
+
+
 
 
 
--- war/war.py
+++ war/war.py
@@ -16,18 +16,31 @@ class War:
         currentBoard = []
         for player in self.players:
             currentBoard.append((player, player.drawFromHand()))
+
+        print("Current board: ")
+        for player, card in currentBoard:
+            print(str(player.name) + " played a " + str(card.value) + " of " + str(card.suite))
+
         maxValue = currentBoard[0]
         for player, card in currentBoard:
             if card.value > int(maxValue[1].value):
                 maxValue = (player, card)
+        
+        for card in currentBoard:
+            maxValue[0].addToPile(card)
+        print("Winner: " + maxValue[0].name + " with a " + str(maxValue[1].value) + " of " + str(maxValue[1].suite))
 
-        counter = 0
-        for player, card in currentBoard:
-            if card.value == maxValue[1].value:
-                counter += 1
-        if counter > 1:
-            self.playRound()
-        return maxValue[0]
+    def winners(self):
+        winners = []
+        maxPileLength = 0
+        for player in self.players:
+            if len(player.pile) > maxPileLength:
+                maxPileLength = len(player.pile)
+        for player in self.players:
+            if maxPileLength == len(player.pile):
+                winners.append(player)
+        return winners
+        
 
             
 
